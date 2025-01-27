a01da26727d46891f31f3605cb34b31c2a1542fe
JoelVRV <joelc456789@gmail.com>
2025-01-28 00:48:01 +0100

    LA API YA FUNCIONA
    - Las rutas se ponen tanto en el api.php como en el web.php, cada archivo tiene sus caracteristica.
    
    - Y para llamar a la ruta desde el thunder, importante poner la ruta asi: http://localhost:8000/api/films
    (con el puerto y lo de /api/)

--- back-end/b-crud-films/routes/web.php
+++ back-end/b-crud-films/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\FilmController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/films', [FilmController::class, 'index']);
